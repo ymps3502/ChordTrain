eca1a64838285aff1ff77c8875a9891803a9097b
MingRen <akihito.ou@gmail.com>
2021-01-23 01:52:32 +0800

    move function to Service

--- src/ClassifyService.php
+++ src/ClassifyService.php
@@ -57,18 +57,73 @@ class ClassifyService
         $paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
         $toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
         $bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
-        train($imagine, 'easy');
-        train($somewhere_over_the_rainbow, 'easy');
-        train($tooManyCooks, 'easy');
-        train($iWillFollowYouIntoTheDark, 'medium');
-        train($babyOneMoreTime, 'medium');
-        train($creep, 'medium');
-        train($paperBag, 'hard');
-        train($toxic, 'hard');
-        train($bulletproof, 'hard');
+        $this->train($imagine, 'easy');
+        $this->train($somewhere_over_the_rainbow, 'easy');
+        $this->train($tooManyCooks, 'easy');
+        $this->train($iWillFollowYouIntoTheDark, 'medium');
+        $this->train($babyOneMoreTime, 'medium');
+        $this->train($creep, 'medium');
+        $this->train($paperBag, 'hard');
+        $this->train($toxic, 'hard');
+        $this->train($bulletproof, 'hard');
 
-        setLabelProbabilities();
-        setChordCountsInLabels();
-        setProbabilityOfChordsInLabels();
+        $this->setLabelProbabilities();
+        $this->setChordCountsInLabels();
+        $this->setProbabilityOfChordsInLabels();
+    }
+
+    function train($chords, $label)
+    {
+        $GLOBALS['songs'][] = [$label, $chords];
+        $GLOBALS['label'][] = $label;
+        for ($i = 0; $i < count($chords); $i++) {
+            if (!in_array($chords[$i], $GLOBALS['allChords'])) {
+                $GLOBALS['allChords'][] = $chords[$i];
+            }
+        }
+        if (!!(in_array($label, array_keys($GLOBALS['labelCounts'])))) {
+            $GLOBALS['labelCounts'][$label] = $GLOBALS['labelCounts'][$label] + 1;
+        } else {
+            $GLOBALS['labelCounts'][$label] = 1;
+        }
+    }
+
+    function getNumberOfSongs()
+    {
+        return count($GLOBALS['songs']);
+    }
+
+    function setLabelProbabilities()
+    {
+        foreach (array_keys($GLOBALS['labelCounts']) as $label) {
+            $numberOfSongs = $this->getNumberOfSongs();
+            $GLOBALS['labelProbabilities'][$label] = $GLOBALS['labelCounts'][$label] / $numberOfSongs;
+        }
+    }
+
+    function setChordCountsInLabels()
+    {
+        foreach ($GLOBALS['songs'] as $i) {
+            if (!isset($GLOBALS['chordCountsInLabels'][$i[0]])) {
+                $GLOBALS['chordCountsInLabels'][$i[0]] = [];
+            }
+            foreach ($i[1] as $j) {
+                if ($GLOBALS['chordCountsInLabels'][$i[0]][$j] > 0) {
+                    $GLOBALS['chordCountsInLabels'][$i[0]][$j] = $GLOBALS['chordCountsInLabels'][$i[0]][$j] + 1;
+                } else {
+                    $GLOBALS['chordCountsInLabels'][$i[0]][$j] = 1;
+                }
+            }
+        }
+    }
+
+    function setProbabilityOfChordsInLabels()
+    {
+        $GLOBALS['probabilityOfChordsInLabels'] = $GLOBALS['chordCountsInLabels'];
+        foreach (array_keys($GLOBALS['probabilityOfChordsInLabels']) as $i) {
+            foreach (array_keys($GLOBALS['probabilityOfChordsInLabels'][$i]) as $j) {
+                $GLOBALS['probabilityOfChordsInLabels'][$i][$j] = $GLOBALS['probabilityOfChordsInLabels'][$i][$j] * 1.0 / count($GLOBALS['songs']);
+            }
+        }
     }
 }
